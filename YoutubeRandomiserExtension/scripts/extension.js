55a803ced33595048318dfca660bc3e99a7add44
Chris Ponticello <chris.developit@gmail.com>
2016-08-27 18:24:30 +1000

    Cleaned up injector code (synchronous, without timers). Cleaned up main body code (readability & error checking).

--- YoutubeRandomiserExtension/scripts/extension.js
+++ YoutubeRandomiserExtension/scripts/extension.js
@@ -1,66 +1,69 @@
 /// <reference path="../../jquery.d.ts"/>
 
-console.log('script load');
+console.info('script load');
 
-var songs = [];
 var api = null;
 var description = null;
+var songs = [];
 var playerBar = null;
+
+var loaded = false;
 var watch = false;
 
-function checkNavigation() {
+function onNavigate() {
+    watch = ('/watch' === location.pathname);
 
-    if ('/watch' === location.pathname) {
+    if (watch) {
         console.log('NAV: watch page!');
-        watch = true;
         init();
+        return;
     }
-    else 
-    {
-        console.log('NAV: non-watch page')
-        watch = false;
-        destroy();
-    }
+    console.log('NAV: non-watch page')
+    destroy();
 }
 
-// try to load the player & the video description
-// delay 1sec while this fails
 function init(attempt = 0){
     if (!watch) {
-        console.log('aborting init');
+        console.warn('INIT: abort (leaving page)');
         return;
     }
 
+    if (attempt > 10) {
+        console.warn('INIT: abort (too many retries)');
+        return;
+    }
+
+    // get player
     var _api = document.getElementById('movie_player');
-    //console.log('api', _api, _api.getCurrentTime);
     if (_api.getCurrentTime === undefined) {
-        console.log('retry');
         window.setTimeout(init, 1000, attempt++);
+        console.warn('INIT: retry (player not found)');
         return;
     }
-
-    console.log('player ready');
     api = _api;
+
+    // get description
+
+    // get songs
+    
+    // create player bar
+
+    loaded = true;
+    console.log('INIT: done!');
 }
 
 function destroy() {}
 
-//$(document).ready(function(){
-    //console.log('document ready');
-//});
-
+function bind() {
     (document.body || document.documentElement).addEventListener(
         'transitionend',
         function(event) {
             if (event.propertyName === 'width' && event.target.id === 'progress') {
-                checkNavigation();
+                onNavigate();
             }
         }, 
         true);
+}
 
-    console.log($('p'));
-    
-    //window.setTimeout(checkNavigation, 5000);
-    checkNavigation();
-
-var hello="hello";
+bind();
+onNavigate();
--- YoutubeRandomiserExtension/scripts/inject.js
+++ YoutubeRandomiserExtension/scripts/inject.js
@@ -1,18 +1,14 @@
 
 // add scripts to web_accessible_resources in manifest.json
 
-var jquery = document.createElement('script'); //type=text/javascript
+function loadScriptSync(src) {
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL(src);
+    s.type = "text/javascript";
+    s.async = false;
+    document.getElementsByTagName('head')[0].appendChild(s);
+    s.onload = function() { s.parentNode.removeChild(s); };
+}
 
-var extension = document.createElement('script'); //type=text/javascript
-
-jquery.src = chrome.extension.getURL('scripts/jquery-3.1.0.js')
-extension.src = chrome.extension.getURL('scripts/extension.js');
-
-(document.head || document.documentElement).appendChild(jquery);
-
-setTimeout(function() {
-    (document.head || document.documentElement).appendChild(extension);
-}, 100);
-
-s1.onload = function() { s1.parentNode.removeChild(s1); };
-s2.onload = function() { s2.parentNode.removeChild(s2); };
+loadScriptSync('scripts/jquery-3.1.0.js');
+loadScriptSync('scripts/extension.js');
